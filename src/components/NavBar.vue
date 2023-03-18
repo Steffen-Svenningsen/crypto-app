000063e416d867a8eb1d4ce3f2103c343ee61b5e
Steffen-Svenningsen <steffen@zynaps.dk>
2023-03-18 15:22:18 +0100

    Create Navigation menu bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <router-link to="/">All</router-link>
+        <router-link to="/top-5">Top 5</router-link>
+        <router-link to="/blog">Blog</router-link>
+    </nav>
+</template>
+
+<style lang="sass" scoped>
+nav
+    width: 100%
+    height: 100%
+    box-sizing: border-box
+    display: flex
+    align-items: center
+    gap: 0.75rem
+
+    a
+        color: white
+        text-decoration: none
+        opacity: 0.5
+        padding: 0.5rem 0.75rem
+        border-radius: 18px
+        text-align: center
+        font-size: 18px
+        display: grid
+        place-items: center
+        background-color: rgba(217, 217, 217, 0.10)
+
+    .router-link-active,
+    .router-link-exact-active
+        border: 1px solid white
+        opacity: 1
+</style>
