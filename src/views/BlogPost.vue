baaae5c207c72ba13a356bb6203de8fd07912f49
Steffen-Svenningsen <steffen@zynaps.dk>
2023-03-24 17:59:40 +0100

    Create blog post article view

---
+++ src/views/BlogPost.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+        <div class="hero-image"></div>
+        <h1>Navigating the Crypto Market</h1>
+        <h3>How to Choose Which Crypto to Invest In</h3>
+        <div class="line"></div>
+        <span>By: <p class="author">Chat GPT</p></span>
+        <p class="desc">As of 2023, there are thousands of cryptocurrencies available to invest in. However, choosing the right one to invest in can be a daunting task, especially with the volatility and unpredictability of the cryptocurrency market. In this article, we will explore some of the factors to consider when deciding which cryptocurrency to invest in.</p>
+        <h6 class="sub-title">Market Capitalization</h6>
+        <p>The market capitalization of a cryptocurrency is the total value of all the coins or tokens in circulation. This is an important factor to consider because it gives an idea of the size of the cryptocurrency. Generally, cryptocurrencies with a high market capitalization are more established and have a higher level of liquidity. Bitcoin, for instance, has a market capitalization of over $1 trillion, making it the largest and most established cryptocurrency in the market.</p>
+        <h6 class="sub-title">Price History</h6>
+        <p>Another important factor to consider is the price history of a cryptocurrency. A cryptocurrency with a stable price history and consistent growth over time is more likely to be a good investment than one with a history of volatility and rapid price swings. However, it is important to note that past performance does not guarantee future results.</p>
+        <h6 class="sub-title">Use Cases and Adoption</h6>
+        <p>A cryptocurrency with real-world use cases and high adoption rates is more likely to have long-term value and sustainability. For example, Ethereum has become a popular platform for building decentralized applications and smart contracts, making it a valuable asset for developers and businesses.</p>
+        <h6 class="sub-title">Community Support</h6>
+        <p>The strength of a cryptocurrency's community is another important factor to consider. A strong community can help drive adoption and awareness, as well as provide valuable insights and support. Bitcoin has a large and passionate community, which has helped to drive its adoption and growth.</p>
+        <h6 class="sub-title">Security</h6>
+        <p>The security of a cryptocurrency is crucial, as it can have a significant impact on its value and sustainability. Cryptocurrencies with strong security measures and protocols are more likely to be trusted by users and investors. For instance, Bitcoin has a robust security system, with its blockchain technology being secure and resilient against attacks. <br> <br> Based on these factors, some of the cryptocurrencies that are worth considering for investment include:</p>
+        <h6 class="sub-title">Bitcoin (BTC)</h6>
+        <p>Bitcoin is the most well-known and established cryptocurrency, with a market capitalization of over $1 trillion. It has a stable price history and a large and passionate community. Additionally, it has a high level of security, making it a reliable investment option.</p>
+        <h6 class="sub-title">Ethereum (ETH)</h6>
+        <p>Ethereum is the second-largest cryptocurrency by market capitalization, with a market capitalization of over $400 billion. It has become a popular platform for building decentralized applications and smart contracts, making it a valuable asset for developers and businesses.</p>
+        <h6 class="sub-title">Binance Coin (BNB)</h6>
+        <p>Binance Coin is the cryptocurrency of the Binance exchange, one of the largest cryptocurrency exchanges in the world. It has a market capitalization of over $80 billion and has seen significant growth in recent years. Additionally, Binance Coin has a strong use case, as it is used to pay transaction fees on the Binance exchange.</p>
+        <h6 class="sub-title">Conclusion</h6>
+        <p>Investing in cryptocurrencies can be risky, but it can also be a lucrative investment option for those who do their research and make informed decisions. When choosing a cryptocurrency to invest in, it is important to consider factors such as market capitalization, price history, use cases and adoption, community support, and security. Bitcoin, Ethereum, and Binance Coin are some of the cryptocurrencies that are worth considering for investment based on these factors. However, it is important to do your own research and make your own decisions based on your investment goals and risk tolerance.</p>
+        <span class="gpt">This was written by an AI (Chat GPT)</span>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+.container
+    padding: 2rem
+    display: flex
+    flex-direction: column
+    background-color: rgba(255, 255, 255, 0.05)
+    border-radius: 18px
+    flex-wrap: wrap
+
+    .hero-image
+        width: 100%
+        height: 340px
+        background-image: url('/crypto.webp')
+        border-radius: 14px
+        background-size: cover
+        background-position: bottom
+        margin-bottom: 2rem
+
+    h1
+        font-size: clamp(20px, 3vw, 28px)
+        font-weight: 700
+
+    h3
+        font-size: 18px
+        margin-top: 1rem
+
+    .line
+        background: linear-gradient(to bottom right, #f8a000 40%, red)
+        width: 40px
+        height: 2px
+        margin: 0.5rem 0
+
+    span
+        display: flex
+        font-size: 12px
+        gap: 0.5rem
+        align-items: center
+
+    .author
+        font-style: italic
+        opacity: 0.80
+
+    .desc
+        margin-top: 2rem
+
+    .sub-title
+        margin: 4rem 0 1.75rem 0
+        font-size: 1.25rem
+        font-weight: 700
+
+    p
+        line-height: 1.6
+        max-width: 90ch
+
+    .gpt
+        opacity: 0.50
+        margin-top: 2rem
+</style>
